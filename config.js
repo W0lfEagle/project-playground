c3185c63916afd879a75d0d6c4a1c60548070718
wilfengel <wilfengel@gmail.com>
2015-11-13 16:35:10 +0000

    add untracked files

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+// Was rtr - restaurant to room
+config.mongoUri = 'mongodb://localhost:27017/students';
+config.cookieMaxAge = 30 * 24 * 3600 * 1000;
+
+module.exports = config;
