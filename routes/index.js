e401ba07ee7665d09a77423b8e13755cfbbd1878
wilfengel <wilfengel@gmail.com>
2015-11-13 00:31:27 +0000

    lingua

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
+
+var transporter = nodemailer.createTransport({
+    service: 'Zoho',
+    auth: {
+        user: "info@linguafrancaworld.com",
+        pass: "passwordtest"
+    }
+});
+    var mailOptions = {
+    from: 'info@linguafrancaworld.com', // sender address 
+    to: 'wilfengel@gmail.com', // list of receivers 
+    subject: 'Hello ✔', // Subject line 
+    text: 'Hello world ✔', // plaintext body 
+    html: '<b>Hello world ✔</b>' // html body 
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if (req.user) {
+        return res.redirect('/users/profile');
+    }
+    var vm = {
+        title: 'Login',
+        error: req.flash('error')
+    }
+    res.render('index', vm);
 });
+
+router.get('/linguafranca', function(req, res, next) {
+    var vm = {
+        title: 'Lingua Franca World',
+        home: true
+    };
+    res.render('linguafranca', vm)
+});
+
+router.post('/linguafranca', function(req, res, next) {
+    transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+ 
+});
+    var vm = {
+        title: 'Create an account',
+        home: false,
+        email: req.body.email
+    };
+  res.render('./users/create', vm);
+});
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,73 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var userService = require('../services/user-service');
+var restrict = require('../auth/restrict');
+var config = require('../config');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET /users/view */
-router.get('/view', function(req, res, next) {
-  res.send('user view');
+/* GET /users/create */ 
+router.get('/create', function(req, res, next) {
+  var vm = {
+    title: 'Create an account',
+    home:  false
+  };
+  res.render('users/create', vm);
 });
+
+/* GET /users/create */
+router.post('/create', function(req, res, next) {
+  // console.log(req.body);
+  userService.addUser(req.body, function(err) {
+    if (err) {
+      console.log(err);
+      var vm = {
+        title: 'Create an account',
+        input: req.body,
+        error: err,
+        home: false
+      };
+      delete vm.input.password;
+      return res.render('users/create', vm);
+    }
+    req.login(req.body, function(err) {
+      res.redirect('/users/profile');
+    });
+  });
+});
+
+router.post('/login', 
+function(req, res, next) {
+    if (req.body.rememberMe) {
+      req.session.cookie.maxAge = config.cookieMaxAge;
+    }
+    next();
+  },
+  passport.authenticate('local', {
+    failureRedirect: '/', 
+    successRedirect: '/users/profile',
+    failureFlash: 'Invalid credentials'
+  }));
+
+router.get('/profile', restrict, function(req, res, next) {
+  // if (!req.isAuthenticated()) {
+  //   return res.redirect('/');
+  // }
+  var vm = {
+    title: 'Welcome to the profile',
+    firstName: req.user ? req.user.firstName : null
+  }
+  res.render('users/profile', vm);
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+})
 
 module.exports = router;
